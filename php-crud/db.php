5e729f38f15204016d87476c8975868f8ae2652e
matalvarez10 <72621530+matalvarez10@users.noreply.github.com>
2022-07-08 02:18:07 -0400

    Add files via upload

---
+++ php-crud/db.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$conn = mysqli_connect('localhost','root','','php_mysql_Crud','3306');
+
+?>
---
+++ php-crud/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+    <!-- BOOTSTRAP 4.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+    crossorigin="anonymous">
+    <!-- FONT AWESOME 5 -->
+    <script src="https://kit.fontawesome.com/bc5a95cc72.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
